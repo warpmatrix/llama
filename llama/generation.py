7ece77213156b2d1d94a161ce3fd05306eeee1e8
warpmatrix <warpmatrixlyk@gmail.com>
2024-06-20 15:46:18 +0800

    profile: init

--- llama/generation.py
+++ llama/generation.py
@@ -268,6 +268,8 @@ class Llama:
         if max_gen_len is None:
             max_gen_len = self.model.params.max_seq_len - 1
         prompt_tokens = [self.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
+        torch.cuda.synchronize()
+        start_time = time.time()
         generation_tokens, generation_logprobs = self.generate(
             prompt_tokens=prompt_tokens,
             max_gen_len=max_gen_len,
@@ -276,6 +278,8 @@ class Llama:
             logprobs=logprobs,
             echo=echo,
         )
+        gen_lens = [len(gen_tokens) for gen_tokens in generation_tokens]
+        print(f"generate {sum(gen_lens)} tokens in {time.time() - start_time}s")
         if logprobs:
             return [
                 {
